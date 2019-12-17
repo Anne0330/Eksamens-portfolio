26c10c7f52122b0dca9b9a72feab273fab5a9521
Anne0330 <55687120+Anne0330@users.noreply.github.com>
2019-12-17 17:44:06 +0100

    Fixed
    CSS virkede ikke så måtte lave noget andet

--- HTML-skelet.php
+++ HTML-skelet.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title>Portfolio</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="CSS%20skelet.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="CSSskelet.css">
         <link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Assistant&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -18,9 +18,49 @@
     </header>
     
     <body>
+        <!-- til top knap-->
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
          <!-- php start-->
     <?php include "menu.inc"; ?>
     <!-- php slut-->
+        <script>
+        //DETTE ER TIL NAVIGATION
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+    //DETTE ER TIL TOP KNAPPEN 
+    //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+</script>
         
         <!-- Her skal kategorierne være som billeder med beskrivelse, hvis man ikke vil bruge menu baren. Det skal stå som titel, info og billede-->
     <section>
@@ -55,7 +95,7 @@
         <p>tilbage til toppen</p>
         <p>kontakt info</p>
     </section>
-        </div>
+
  
     </body>
     
