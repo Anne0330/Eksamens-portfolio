0794c17a3d34046ab60bf140addbe867e489f5d8
Anne0330 <55687120+Anne0330@users.noreply.github.com>
2019-12-20 22:02:01 +0100

    tags tilføjet

--- attributter.php
+++ attributter.php
@@ -2,18 +2,18 @@
 <html lang="da">
     <head>
         <meta charset="utf-8">
-        <title>HTML</title>
+        <title>Tags</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="html.css">
+        <link rel="stylesheet" href="tagscss.css">
         <link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Assistant&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <header>
-        <h1>HTML</h1>
+        <h1>Tags</h1>
         
         <div id="introtekst">
-             <p>Html betyder HyperText Markup Language, som er et kodesprog. Dens formål bruges til at ’markere’ tekst og billeder på en hjemmeside. Det er et sprog med struktur, og ligeledes kan det også have hierarki. På denne side kan du læse lidt om HTML, kodeskik</p>
+             <p>Her kan du læse om tags i forhold til webudvikling. Du vil kunne læse om almindelige tags, samt hvad semantiske tags er og deres forhold til hvorfor de er vigtige til at udivkle en hjemmeside.</p>
         </div>
     </header>
     
@@ -67,42 +67,45 @@
         <div id="htmltitel"> 
             <img src="htmltitelbillede1.jpg" alt="eye code" height="200px">
             </div>
-            <div id="htmltekst">
-                <h2>Html</h2>
-                    <p>Under vores læringsproces har vi kendskab til forskellige editors, såsom: Notepad++, Edge Code, Brackets, TextWrangler, Sublime Text, Atom, Ultra Edit, BBEdit og mange andre. Vi har startet ud med at bruge Brackets og eventuelt i fremtiden, bevæger vi os ind i andre editors.</p>
-                    <p>Det man skriver ind i sit HTML, bliver som regel kaldt for elementer, som har egenskaber heraf tags, attributter og værdier mm. Alle elementer bliver oftest startet med et tag, og slutter med et tag.</p>
+            <div id="tagstekst">
+                <h2>Tags</h2>
+                    <p>Tags er i bund og grund elementer, som vi kalder for ”kode” i hverdagens sprog. Tags er symboler med tegnet ”&lt;" og ”&gt;”, så et tag kan således se sådan her ud: &lt;love&gt; 
+                    </p>
+                <p>Tags som &lt;section&gt; definerer sektioner i et html dokument, såsom kapitler, intro, footer osv. Det giver et overblik for udvikler og læser, på hvad der er ment til at blive sammensat.</p>
+                    <p>Semantiske tags/elementer, er elementer som har en mening. Det giver en forståelse eller beskrivelse, for både browseren og udvikleren af hvad inholdet er.</p>
+                <p>Eksempler på ikke-semantiske tags er: &lt;div&gt; og &lt;span&gt; - fordi de ikke fortæller om indholdet.</p>
+                <p>Eksempler på semantiske tags er: &lt;form&gt;, &lt;table&gt; og &lt;article&gt; - fordi de fortæller hvad indholdet er.</p>
         </div>
-            <div id="kilderhtml">
+            <div id="kildertags">
                 <p> Kilde: Niels Østergaard powerpoint præsentation "Introduktion til HTML".<br>
                 <a href="https://www.w3schools.com/">Kilde: w3schools</a></p> 
             </div>
-               <div id="htmlelement">
-            <img src="html-element.png" alt="HTML element" width="200px">
+               <div id="semantiskbill">
+            <img src="semantisketags.png" alt="semantiske elementer" width="200px">
                    </div> 
-                <div id="htmlkilde">
-                <a href="https://docs.code.org/csd/html_tags/index.html">Kilde</a>
+                <div id="tagskilde">
+                <a href="https://medium.com/teleporthq-io/understanding-the-web-parsing-web-pages-semantically-805ef857854d">Kilde</a>
                 </div>
-                
-                <h2>Kodeskik</h2>
-
-            <div id="kodeskiktekst">
-                <p>Udvikleren skal have et overblik, samt skal en kunde eller udviklings-partner kunne læse din kode uden problemer. Det vil sige, man skal navngive sine ID elementer der er i div tagget, i stedet for at bruge tal</p>
-                
-                <img src="div-id.png" alt="Id elementer" width="100px">
-                
-            <div id="kildeid">
-                <a href="https://www.ediciones-eni.com/open/mediabook.aspx?idR=2f06db4bea8b96b852047e02d72420c0">Kilde</a>
+                <h2>Andre semantiske tags</h2>
+            <div id="tagseksempler">
+                <ul>
+                    <li>&lt;h1&gt;, &lt;h2&gt;,&lt;h3&gt;... - Titler/h1 er størst og bør kun bruges én gang på en side.</li>
+                    <li>&lt;p&gt; = Paragraf</li>
+                    <li>&lt;ol type="A/1/I"&gt; = 'Ordered list', Nummer, alfabet liste</li>
+                    <li>&lt;ul&gt;&lt;/ul&gt; = punkteret liste</li>
+                    <li>&lt;ul>&lt;/ul> = punkteret liste uden orden. 'un-ordered list'</li>
+                    <li>&lt;Li>Et punkt&lt;/li> = Et punkt på en liste.</li>
+                    <li>&lt;a href=""> Link til diverse hjemmesider eller andre html dokumenter.</li>
+                    <li>&lt;dl> er en beskrivelses liste. Det er brugt til at definere et ord, ligesom når man skal finde en beskrivelse på et ord. Fx ordbøger</li>
+                    <li>&lt;dt> bruges sammen med &lt;dl> tagget, da det bruges til at 'highlight' ordet man foreksemepel gerne vil have en beskrivelse på.</li>
+                    <li>&lt;dd> bruges til at beskrive ordet du har valgt i dit &lt;dt></li>
+                    <li>&lt;img src="img_girl.jpg" alt="Girl in a jacket". Bruges til billeder.</li>
+                </ul>
             </div>
                 
-                <p>Her kan en kunde/læser gennemskue hvad elementet er til, uden at de skal gå længere ind i kodens indhold for at få en forståelse. Kommentarer er også dyrebare i ens kodning, da det er en hjælpeguide for selve udvikleren og læseren. Et eksempel nedenfor er fra min egen kode, for denne side:</p>
-            </div>
-          <div id="kilderhtml">
+          <div id="kildertags">
                 <p> Kilde: Niels Østergaard powerpoint præsentation "Introduktion til HTML".<br>
                 <a href="https://www.w3schools.com/">Kilde: w3schools</a></p> 
-            </div>
-        
-            <div id="kommentarbill">
-                <img src="kommentar.png" alt="kommentarer" width="800px">
             </div>
         
         <div class="footer">
