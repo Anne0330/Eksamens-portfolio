16a3b28ddeb93159af19f2ee410bc37a8e41b6d4
Anne0330 <55687120+Anne0330@users.noreply.github.com>
2019-12-20 22:33:07 +0100

    attributter done

--- attributter.php
+++ attributter.php
@@ -4,16 +4,16 @@
         <meta charset="utf-8">
         <title>Tags</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="tagscss.css">
+        <link rel="stylesheet" href="attritbuttercss.css">
         <link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Assistant&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <header>
-        <h1>Tags</h1>
+        <h1>Attributter</h1>
         
         <div id="introtekst">
-             <p>Her kan du læse om tags i forhold til webudvikling. Du vil kunne læse om almindelige tags, samt hvad semantiske tags er og deres forhold til hvorfor de er vigtige til at udivkle en hjemmeside.</p>
+             <p>Her kan du læse om hvad attributter er, og hvordan det har betydning i forhold til elementet.</p>
         </div>
     </header>
     
@@ -68,45 +68,24 @@
             <img src="htmltitelbillede1.jpg" alt="eye code" height="200px">
             </div>
             <div id="tagstekst">
-                <h2>Tags</h2>
-                    <p>Tags er i bund og grund elementer, som vi kalder for ”kode” i hverdagens sprog. Tags er symboler med tegnet ”&lt;" og ”&gt;”, så et tag kan således se sådan her ud: &lt;love&gt; 
-                    </p>
-                <p>Tags som &lt;section&gt; definerer sektioner i et html dokument, såsom kapitler, intro, footer osv. Det giver et overblik for udvikler og læser, på hvad der er ment til at blive sammensat.</p>
-                    <p>Semantiske tags/elementer, er elementer som har en mening. Det giver en forståelse eller beskrivelse, for både browseren og udvikleren af hvad inholdet er.</p>
-                <p>Eksempler på ikke-semantiske tags er: &lt;div&gt; og &lt;span&gt; - fordi de ikke fortæller om indholdet.</p>
-                <p>Eksempler på semantiske tags er: &lt;form&gt;, &lt;table&gt; og &lt;article&gt; - fordi de fortæller hvad indholdet er.</p>
-        </div>
-            <div id="kildertags">
-                <p> Kilde: Niels Østergaard powerpoint præsentation "Introduktion til HTML".<br>
-                <a href="https://www.w3schools.com/">Kilde: w3schools</a></p> 
-            </div>
-               <div id="semantiskbill">
-            <img src="semantisketags.png" alt="semantiske elementer" width="200px">
-                   </div> 
-                <div id="tagskilde">
-                <a href="https://medium.com/teleporthq-io/understanding-the-web-parsing-web-pages-semantically-805ef857854d">Kilde</a>
-                </div>
-                <h2>Andre semantiske tags</h2>
-            <div id="tagseksempler">
-                <ul>
-                    <li>&lt;h1&gt;, &lt;h2&gt;,&lt;h3&gt;... - Titler/h1 er størst og bør kun bruges én gang på en side.</li>
-                    <li>&lt;p&gt; = Paragraf</li>
-                    <li>&lt;ol type="A/1/I"&gt; = 'Ordered list', Nummer, alfabet liste</li>
-                    <li>&lt;ul&gt;&lt;/ul&gt; = punkteret liste</li>
-                    <li>&lt;ul>&lt;/ul> = punkteret liste uden orden. 'un-ordered list'</li>
-                    <li>&lt;Li>Et punkt&lt;/li> = Et punkt på en liste.</li>
-                    <li>&lt;a href=""> Link til diverse hjemmesider eller andre html dokumenter.</li>
-                    <li>&lt;dl> er en beskrivelses liste. Det er brugt til at definere et ord, ligesom når man skal finde en beskrivelse på et ord. Fx ordbøger</li>
-                    <li>&lt;dt> bruges sammen med &lt;dl> tagget, da det bruges til at 'highlight' ordet man foreksemepel gerne vil have en beskrivelse på.</li>
-                    <li>&lt;dd> bruges til at beskrive ordet du har valgt i dit &lt;dt></li>
-                    <li>&lt;img src="img_girl.jpg" alt="Girl in a jacket". Bruges til billeder.</li>
-                </ul>
-            </div>
+                <h2>Attributter</h2>
+                    <p>Attributter er giver ekstra information til elementet, og er altid nævnt som det første i dit start tag. Som regel kommer attributer som “name/value” par såsom: Name=”value”.<br>
+                    I HTML kan man bruge et &lt;imh> tag, til at få billeder frem. Tagget er i teorien tom, da der ikke er et lukke tag og den kun har en attribut i sit element. Ved at tilføje "src" ind i elementet, kan man linke til et billede med URL. Derved kan man tilføje "alt" ind i elementet, der gør at hvis brugeren ikke kan se billedet, kommer der en tekst frem som er inde i "alt" tagget.</p>
                 
-          <div id="kildertags">
+                    <p>Du får så derfor fx:<br> &lt;img_flower.jpg" alt="blomster der er farverige"</p>
+                
+                    <p>I bund og grund, gør attributter at det giver flere informationer for brugeren på hvad elementet indebærer.</p>
+        </div>
+            <div id="kilderattribut">
                 <p> Kilde: Niels Østergaard powerpoint præsentation "Introduktion til HTML".<br>
                 <a href="https://www.w3schools.com/">Kilde: w3schools</a></p> 
             </div>
+               <div id="attributbill">
+            <img src="attributterbill.png" alt="attributter" width="200px">
+                   </div> 
+                <div id="attributkilde">
+                <a href="https://www.onlinedesignteacher.com/2016/03/html-attributes.html">Kilde</a>
+        </div>        
         
         <div class="footer">
             <p>Anne Mehlsen &#169; Læringsportfolio 2019-</p>
