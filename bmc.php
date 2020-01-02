f475c754e19202af14dd95923ff8da99f4b5a108
Anne0330 <55687120+Anne0330@users.noreply.github.com>
2020-01-02 18:23:24 +0100

    bmc done

---
+++ bmc.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <title>BMC</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bmccss.css">
+        <link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Assistant&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link href="https://fonts.googleapis.com/css?family=Cormorant+Upright&display=swap" rel="stylesheet">
+    </head>
+    <header>
+        <h1>Business model canvas</h1>
+        
+        <div id="introtekst">
+             <p>Her kan du læse om Business model canvas - BMC, som forklarer faktorerne indenfor virksomhedsforståelse.</p>
+        </div>
+    </header>
+    
+    <body> 
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+       
+           <!-- php start-->
+        <?php include "menu.inc"; ?>
+            <!-- php slut-->
+        
+        <div id="htmltitel"> 
+            <img src="communication.jpg" alt="Design" height="200px">
+            </div>
+        
+        <div id="bmc">
+                
+                <h2>BMC – model</h2>
+            
+            <img src="bmc.png" alt="bmc" width="70%">
+                
+                <p>En forretningsmodel er vigtig i forhold til at kunne forstå virksomheden man arbejder med, eller sin egen virksomhed. Hvordan de engagerer sig med sine kunder, kundefordele, ressourcer, nøglepartnere mm. Det er et strategisk ledelsesværktøj, der kan anvendes i forhold til strategisk anvendelse, planlægning og udvikle virksomhedens forretningsmodel.</p>
+                
+                <h3>De 7 arketyper</h3>
+            
+               <dl>
+                    <dt>1. Den digitale købmand</dt>
+                    <dd>Har sin fysiske butik men supplerer med salg via Internettet.<br>
+                        Branding, annoncering, varekataloger, service- og vejledninger på nettet.</dd>
+                    <dt>2. Webshoppen</dt>
+                    <dd>Køber hos grosister og sælger direkte til forbrugere via internettet.<br>Undgår fordyrende fysisk detailbutik omkostninger og har samtidigt større varesortiment.</dd>
+                    <dt>3. Den digitale producent</dt>
+                    <dd>Udvider sine fysiske produkter med digitale tjenester bl.a. med Internet Of Things.<br>Skaber nye funktionaliteter samt digitale lock-in effekter om det fysiske produkt.</dd>
+                    <dt>4. Software producenten</dt>
+                    <dd>Bygger software med licens/direkte betaling.<br>Marginalomkostninger grænsende til nul, skalerbarhed og udrulning/opdatering via internet.</dd>
+                    <dt>5. Den digitale platformsbygger</dt>
+                    <dd>Bygger sin egen digitale platform på internettet. <br>Skalering af antal bruger og brugen kilden til indirekte salg.</dd>
+                   <dt>6. App opfinderen</dt>
+                   <dd>Ser et behov og bygger en app som dækker behovet.<br>Ekstremt lave start-up og exit costs.</dd>
+                   <dt>7. Wiki-skaberen</dt>
+                   <dd>Brugerne er både skabere samt forbrugere af indholdet.<br>Indtjening sker gennem donationer og eller indirekte salg.</dd>
+            </dl>
+            
+            <h3>Der er tre forskellige virksomhedstyper:</h3>
+            
+            <dl>
+                <dt>Produktionsvirksomheder</dt>
+                <dd>Fremstiller/forarbejder varer.</dd>
+                <dt>Handelsvirksomheder</dt>
+                <dd>Køber og sælger varer.<br>Ingen forarbejdning.<br>Engros og detail.</dd>
+                <dt>Servicevirksomheder</dt>
+                <dd>Sælger uhåndgribelige varer/tjenesteydelser.<br>Varen/ydelsen kan ikke lagres.</dd>
+            
+            </dl>
+            
+            <h3>En businessmodel har tit tre fremstillinger som er:</h3>
+            
+            <dl>
+                <dt>Mission - Et overordnet formål for virksomheden</dt>
+                <dd>Klart formuleret formål, realistisk, tjener alle interessenters interesse, klar differentiering ift. konkurrenter.</dd>
+                <dd>Midlerne til at nå vision.</dd>
+                <dd>Hvad vil man konkurrere på.</dd>
+                <dd>En mission laves for at skabe en generel forståelse af, hvorfor organisationen eksisterer, og hvad den vil opnå.</dd>
+                <dt>Vision - Visionen handler om, hvor man vil have virksomheden hen – ønskværdigt eller forventet.</dt>
+                <dd>Langsigtet.</dd>
+                <dd>Visionen beskriver et fremtidigt billede af virksomheden. Det er den fælles retning, som hele virksomheden skal arbejde efter at opnå.</dd>
+                <dt>Værdier</dt>
+                <dd>Værdigrundlagets funktion er at påvirke holdninger i organisationen/virksomheden.</dd>
+            
+            </dl>
+            
+            
+            <img src="bmcforskellen.png" alt="bmc og bm forskel" width="70%">
+            
+        
+        </div>
+        
+        <div id="businessmoden">
+            <h2>Business model canvas - Forståelsen</h2>
+            
+            <img src="businessmodelcanvas.png" alt="business model canvas" width="70%">
+            
+            <p>Value proposition/værdifaktorer er vigtigt i en BMC, da det afgør hvordan dit produkt eller service hjælper dine kunders problemer. Hvilken værdi skabes for kunderne? Hvilke problemer bliver løst? Hvilke af kundernes behov opfyldes? Det giver et overblik, på hvad dine produkter gør i forvejen. Med en kvalitativ eller kvantitativ undersøgelse, kan man med fordel undersøge sine value propositions.</p>
+            
+            <p>Aktiviteter, partnere og ressourcer er hvordan man vil opnå disse værdifaktorer. Aktiviteter kan være konkurrencer, spørgeskemaer eller lign. For at trække sine kunder ind og være en del af at optimere forretningen. Partnere kan såvel være leverandører, til det er andre virksomheder hvor dit produkt sælges. Hvad gør partnerne for at hjælpe med værdifaktorerne? Ressource er selve ressourcerne som gør der skabes en værdi for kunderne.</p>
+            
+            <p>Kunderne er dem man skaber værdi for, og det er vigtigt at vide hvem de er. Hvilke er de mest vigtige kunder? Hvilke kundesegmenter kan defineres</p>
+            
+            <p>Kunderelationer er hvordan man interagerer med sine kunder. Hvilken påvirkelse har viskomheden på sine kunder? Hvordan vedligeholder man relationer?</p>
+            
+            <p>Vejen til kunderne er hvordan man opnår en kommunikation med kunderne. Gennem hvilke kanaler foretrækker kunderne at modtage kommunikation? Hvordan er kommunikation i dag? Hvad fungere bedst, i forhold til kommunikationen med sine kunder?</p>
+            
+            <p>Omkostninger er prisen for sine værdifaktorer, om hvor meget der blive brugt for at opnå en værdifaktor. Hvor er de væsentlighe omkostninger i forretningsmodellen?Hvilke ressourcer er mest omkostningstunge? Hvilke aktiviteter er mest omkostningstunge?</p>
+            
+            <p>Indtægter er det der kommer tilbage fra værdi faktorernes resultat. Hvordan vil kunderne betale? Hvor meget bidrager hver indtægtskilde til den totale omsætning? Hvor meget er de villige til at betale?</p>
+            
+        </div>
+       
+        
+        <div id="kilder">
+                <h5>Øvrige kilder</h5>
+                    <p>Morten Kluw Wøldike Schmith powerpoint præsentation "BMC" - 9/10/2019<br>
+                        
+                    "Den digitale virksomhed – Syv arketyper", 10/11/2014, ERHVERVSSTYRELSEN<br>
+                        
+                    "Business Model Generation" 2015, Alexander Osterwalder og Yves Pigneur<br>
+                        
+                    Billeder: Morten Kluw Wøldike Schmith powerpoint præsentation "BMC" - 9/10/2019<br>
+                    
+                    <a href="https://www.youtube.com/watch?v=WzGhFmiB9G0">"Airbnb Business Model : What makes Airbnb so successful?", 4/8/2017, Business Disruptors</a>
+                        
+                    <a href="https://www.youtube.com/watch?v=IP0cUBWTgpY">"The Business Model Canvas - 9 Steps to Creating a Successful Business Model - Startup Tips", 5/9/2016, The Business Channel</a>
+                     
+                    <a href="https://www.youtube.com/watch?v=QgICyLzZ3og">"Business Model Canvas - et værktøj til at udvikle og disrupte forretningsmodeller", 5/1/2017, Josefine Campbell</a></p>
+            </div>
+        
+        <div class="footer">
+            <p>Anne Mehlsen &#169; Læringsportfolio 2019-</p>
+        </div>
+        
+        <script>
+        //DETTE ER TIL NAVIGATION
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+        var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+
+        for (i = 0; i < dropdown.length; i++) {
+          dropdown[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var dropdownContent = this.nextElementSibling;
+          if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+          } else {
+          dropdownContent.style.display = "block";
+          }
+          });
+        }
+            //DETTE ER TIL TOP KNAPPEN 
+            //Get the button:
+        mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+          } else {
+            mybutton.style.display = "none";
+          }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+          document.body.scrollTop = 0; // For Safari
+          document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+        
+          
+    </script>
+
+    </body>
+</html>
