92761afc37725b05b88c66f57fb562241a32d37b
Anne0330 <55687120+Anne0330@users.noreply.github.com>
2020-01-02 16:34:32 +0100

    tekstproduktion færdig

---
+++ tekstproduktion.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <title>Tekstproduktion</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="tekstcss.css">
+        <link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Assistant&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link href="https://fonts.googleapis.com/css?family=Cormorant+Upright&display=swap" rel="stylesheet">
+    </head>
+    <header>
+        <h1>Tekstproduktion</h1>
+        
+        <div id="introtekst">
+             <p>Her kan du læse om hvordan man producere en god tekst til artikler, nettet mm.</p>
+        </div>
+        
+    </header>
+    
+    <body> 
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+       
+           <!-- php start-->
+        <?php include "menu.inc"; ?>
+            <!-- php slut-->
+        
+        <div id="htmltitel"> 
+            <img src="communication.jpg" alt="Design" height="200px">
+            </div>
+        
+        <div id="genre">
+                
+                <h2>Genre</h2>
+                
+                <p>Udover at kende til brugeren eller læseren, er det vigtigt at kunne skrive den rigtige tekst til kanalen. Der er struktur, i forhold til hvor teksten er skrevet til. Hvilket afgør hvordan strukturen ser ud for teksten, og hvordan den skal skrives for at finde læseren.</p>
+                
+                <h3>Der er forskellige typer for medier/genre er:</h3>
+            
+                <dl>
+                    
+                    <dt>Artikel</dt>
+                    <dd>En klassisk artikelstruktur, som man ser i avisen/magasinet.</dd>
+                    <dt>Brochure</dt>
+                    <dd>En informationsstruktur, som giver en form for information eller oplysninger.</dd>
+                    <dt>Annonce</dt>
+                    <dd>En traditionel opbygning.</dd>
+                    <dt>Webtekster</dt>
+                    <dd>Som regel en ’hurtig’ tekst til den ’travle læser’. Webtekster skrives som regel, ikke ligesom en tryktekst.</dd>
+                    <dt>Sociale medietekster</dt>
+                    <dd>Spontanitet og hurtig tekst. Den fanger en bestemt målgruppe.</dd>
+                    
+                </dl>
+        </div>
+        
+        <div id="kriterier">
+
+       <h3>De fem nyhedskriterier</h3>
+            
+            <p>Man anvender de fem nyhedskriterier som en guide, som er med til at bestemme værdien for nyheden. Man anser at en nyhedsartikel opfylder 2-3 af nyhedskriterierne, da man kan få 3 ting ud af det som er, Kvalitet, delinger og links.</p>
+            
+             <dl>
+                <dt>Væsentlighed</dt>
+                <dd>At noget har en grundlæggende betydning eller konsekvens for mange mennesker.<br> Ex: Skatten stiger næste år.</dd>
+                <dt>Sensation</dt>
+                <dd>Noget der fascinerer eller chokerer. Ofte de små, skæve historier i nogle aviser, som kan være forsidehistorier i andre aviser.<br>Ex: Mand bider hund eller Kvinde overlevede en måned under sammenstyrtet hus.</dd>
+                <dt>Konflikt</dt>
+                <dd>Når sager eller personer er på kollisionskurs. Ex: Vred far anklager kommune for sjusk.</dd>
+                <dt>Identifikation</dt>
+                <dd>Noget som læseren kan genkende sig selv i. En historie, der får læseren til at tænke ’Det kunne være mig’, ’Bare det var mig’ eller ’Godt, det ikke er mig’.<br>Ex: Ni ud af ti danskere spiser for meget sukker. </dd>
+                <dt>Aktualitet</dt>
+                <dd>Noget der er oppe i tiden. Historien må gerne udspringe af stof som i forvejen cirkulerer i andre medier. Langt de fleste nyheder udspringer af dette nyhedskriterium. Ex: Et parti vælger ny formand eller et containerskib er sejlet ind i en bro.</dd>
+            </dl>
+            
+            <h4>AIDA modellen</h4>
+            
+            <img src="aida.png" alt="aida Model" width="70%">
+            
+            <p>AIDA modellen bruges som regel til plakater, reklamationer, videoer og opslag for at få modtagerens opmærksomhed.</p>
+            
+            <dl>
+                <dt>Attention</dt>
+                <dd>Positiv opmærksomhed<br>Relevans for modtager</dd>
+                <dt>Interest</dt>
+                <dd>Fastholde interessen for sagen/teksten, produkt eller budskab.</dd>
+                <dt>Desire</dt>
+                <dd>Skab et behov<br>Eventuelt bruger følelser.</dd>
+                <dt>Action</dt>
+                <dd>Hvad skal modtageren gøre, i forhold til teksten.<br>Eks. Købe produktet der er reklameret for</dd>
+            </dl>
+            
+            <p>Der skal ligeledes skrives en tekst der passer til indholdet, og få opmærksomheden fra målgruppen. Derved kan man bruge følgende strategier, som kan blandes og passes ind i forhold til billeder, grafik og andre medier.</p>
+            
+            <dl>
+                <dt>USP/Unique Selling Proposition</dt>
+                <dd>Denne bruges til tilbud der er ’lige nu og her’ eller kvalitet, udbud, sted og lignende.</dd>
+                <dt>ESP/Emotional Selling Proposition</dt>
+                <dd>Man forsøger at komme i kontakt med følelserne med en pathos strategi, der kan enten være godt eller ondt.</dd>
+                <dt>ISP/Ironic Selling Proposition</dt>
+                <dd>Man bruger ironi eller en humor der kan virke stødende, eks. Sort humor.</dd>
+                <dt>BSP/Brand Selling Proposition</dt>
+                <dd>Et mærke med høj rank i branchen, i forhold til det produkt der bliver solgt.</dd>
+                <dt>OSP/Orginizational Selling Proposition</dt>
+                <dd>Mærker som er værdifulde, vil betyde at produkter bliver solgt baseret på navnet har værdi.</dd>
+            </dl>
+            
+
+        </div>
+    
+    <div id="trekanten">
+            
+            <h2>Nyhedstrekanten generelt</h2>
+        
+            <img src="nyhedstrekant.jpg" alt="nyhedstrekanten" width="70%">
+        
+                <ul>
+                    <li>Indledningen<br>Fortæller nyheden - Hvem, hvad, hvor, hvornår?</li>
+                    <li>Uddybning<br>Information som forklarer og udvider indledningen.</li>
+                    <li>Baggrund.</li>
+                    <li>Sekundære emner.</li>
+                    <li>Detaljer.</li>
+                </ul>
+        
+            <h4>Eksempel på nyhedstrekanten i brug</h4>
+        
+        <img src="trekantibrug.png" alt="nyhedstrekant i brug" width="70%">
+    </div>
+        
+    <div id="nyhednettet">
+        
+        <h3>Den gode tekst på nettet</h3>
+        
+        <dl>
+            <dt>Hovedbudskab</dt>
+            <dd>Konklusion skal skrives i overskriften, da det skal være hurtigt og læse.</dd>
+            <dt>Let at skimme/baggrund</dt>
+            <dd>Folk har som regel travlt og hvis det de leder efter, ikke er indenfor de første to linjer, skipper de videre. Teksten skal være nem, overskuelig og ”lige til”.</dd>
+            <dt>Skrevet i lag</dt>
+            <dd>Detaljerne for baggrund.</dd>
+        </dl>
+        
+        <h3>De fire sprogdyder</h3>
+        
+        <dl>
+            <dt>Aptum – hensigtsmæssighed = det passende sprog</dt>
+            <dd>Betegner talens tilpasning til alle de forhold, der har betydning for, om afsenderen opnår sit mål med talen.<br>Emnet, modtager/målgruppe, modtagers forhåndsviden og indstilling til emnet.<br>”Adressatrelevans”; situationalitet og acceptabilitet.</dd>
+            <dt>Puritas - Renhed = Korrekthed</dt>
+            <dd>Puritas drejer sig om at overholde sprogets regler for korrekthed med hensyn til grammatik, udtale, stavning, syntaks osv.<br>Kan ‘oversættes’ til sprogrigtighed.</dd>
+            <dt>Ornauts - Udsmykning = Giv teksten liv</dt>
+            <dd>Sproglig udsmykning, der skal gøre sproget mere levende, personligt og vedkommende.<br>Ornauts skal ikke forstås blot som sproglig udsmykning.<br>En levende og overbevisende fremstilling skal anskueliggøre emnet, gøre det nærværende og personligt vedkommende.<br>Hvis sproget er korrekt og klart – og meget præcist, så kan det tit blive kedeligt.<br>Der skal pynt til: Metaforer, billeder, ordvalg, syntaks mm.</dd>
+            <dt>Perspicuitas: Gennemsigtighed = Klarhed</dt>
+            <dd>Tekstens sproglige klarhed og dens gennemskuelighed. Det er tekstens perspicuitas, der sikrer dens forståelighed.</dd>
+        </dl>
+
+        <h3>Sinful.dk</h3>
+        
+        <p>Under første semester skulle vi skrive en opgave, der gør brug af de tre nyhedsartikler: Web, nyhedsartikel og sociale medie tekst. Der skulle vælges en virksomhed, som vi skulle involvere i de tre genre. Jeg valgte <a href="https://www.sinful.dk/">Sinful.dk</a>, til at være den virksomhed jeg skulle skrive for. Du kan læse min opgave, ved at trykke <a href="sinful%20nyhedsartikler.pdf" target="_blank">her.</a></p>
+            
+    </div>
+        
+           
+        <div id="kilder">
+                <h5>Øvrige kilder</h5>
+            
+                    <p>Billeder: Morten Kluw Wøldike Schmith powerpoint præsentation "Tekstproduktion" - 4/10/2019<br>
+                        
+                    Morten Kluw Wøldike Schmith powerpoint præsentation "Tekstproduktion" - 4/10/2019<br>
+                        
+                    "Online kommunikation", Nanna Friis, kapitel 5<br>
+                        
+                    "Kommunikation i multimediedesign" 2015, Anne Mette Busch, kapitel 17<br>
+                     
+                    <a href="21_sproglige_dodssynder_v2.pdf">"21 sproglige dødssynder", Bro Kommunikation</a></p>
+            </div>
+        
+        <div class="footer">
+            <p>Anne Mehlsen &#169; Læringsportfolio 2019-</p>
+        </div>
+        
+        <script>
+        //DETTE ER TIL NAVIGATION
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+        var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+
+        for (i = 0; i < dropdown.length; i++) {
+          dropdown[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var dropdownContent = this.nextElementSibling;
+          if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+          } else {
+          dropdownContent.style.display = "block";
+          }
+          });
+        }
+            //DETTE ER TIL TOP KNAPPEN 
+            //Get the button:
+        mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+          } else {
+            mybutton.style.display = "none";
+          }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+          document.body.scrollTop = 0; // For Safari
+          document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+        
+          
+    </script>
+
+    </body>
+</html>
