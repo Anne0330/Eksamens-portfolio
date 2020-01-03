8f8ee9f5b2143d528ae2662d2333dc32687e95df
Anne0330 <55687120+Anne0330@users.noreply.github.com>
2020-01-03 23:49:36 +0100

    forside færdig

--- startside.php
+++ startside.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Forside</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="fcss.css">
+        <link rel="stylesheet" href="startside.css">
         <link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Assistant&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -13,8 +13,9 @@
     <header>
         <h1>Anne's portfolio</h1>
         <!-- malplaceret undertitel???-->
-        <p>Dette er mit portfolio, der viser hvad jeg har lært på første semester hos Erhvervsakademiet Dania. Brug navigationsbaren til at navigere rundt på siden, da det tog mig flere timer i ren frustration og højt blodtryk, at få den op og køre.</p>
+        <p>Dette er mit portfolio, der viser hvad jeg har lært på første semester hos Erhvervsakademiet Dania som Multimediedesigner. Brug navigationsbaren til at navigere rundt på siden, da det tog mig flere timer i ren frustration og højt blodtryk, at få den op og køre.</p>
     </header>
+    
     
         <body>
         
@@ -29,19 +30,19 @@
     <!-- php slut-->
         
         <div id="billede">
-            <img src="chillforside-1.jpg" alt="profil" width="70%">
+            <img src="chillforside.jpg" alt="profil" width="70%">
         </div>
         
             <div id="tekst">
-                <p>Tekst TekstTekstTekstTek</p>
+                <p>I dette portfolio kan du læse om de forskellige fag, som vi har skulle arbejde med i teori og praktisk. Det omhandler webudvikling som er html, css, informations arkitektur, SEO mm. Vi har også arbejdet med virksomhedsforståelse, hvor vi har skulle analysere modeller indenfor Business model canvas/BMC, paradigmer, brugertest mm. I design har vi øvet os i photoshop, illustrator og teoretisk viden omkring farver, typografi, gestaltlovene mm. Derudover kan du læse og se vores projekter vi har lavet i samarbejde med hinanden i grupper, som gælder primært om at få lavet et produkt eller prototype. Der er rig mulighed for at læse et afsnit omkring mine reflektioner ved at arbejde med dette portfolio, hvor jeg fik afklaret hvor meget denne uddannelse har lært mig siden September 2019.</p>
             
             </div>
            
         <div class="footer">
             <p>Anne Mehlsen &#169; Læringsportfolio 2019-</p>
         </div>
-        
-       <script>
+            
+            <script>
         //DETTE ER TIL NAVIGATION
 /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
         var dropdown = document.getElementsByClassName("dropdown-btn");
@@ -85,3 +86,5 @@
     </body>
     
 </html>
+        
+    
